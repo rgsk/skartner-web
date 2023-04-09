cc8ef292d6e058b87ce824083852ccc6173ed1df
rgsk <guptarahul70322@gmail.com>
2023-04-10 01:00:31 +0530

    dockerfile setup

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -2,15 +2,17 @@ import { Button, Typography } from "@mui/material";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 import styled from "@emotion/styled";
+import environmentVars from "lib/environmentVars";
 
 const StyledButton = styled.button`
   color: turquoise;
 `;
 interface IPracticePageProps {}
 const PracticePage: React.FC<IPracticePageProps> = ({}) => {
+  console.log(environmentVars);
   return (
     <div>
-      <h1 className="text-red-400">this is the page changed</h1>
+      <h1 className="text-red-400">this is the page changed 12345</h1>
       <div className="flex">
         <Button variant="contained">hii</Button>
         <div className="ml-2">
