b2138f7ace5c7fce7b26708653c98b23f468a7d2
rgsk <guptarahul70322@gmail.com>
2023-04-13 10:51:56 +0530

    simple apollo integration

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,15 +1,33 @@
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import { Button, Typography } from '@mui/material';
 
+import { gql, useQuery } from '@apollo/client';
 import styled from '@emotion/styled';
 import environmentVars from 'lib/environmentVars';
 
 const StyledButton = styled.button`
   color: turquoise;
 `;
+
+const DraftsDocument = gql`
+  query {
+    drafts {
+      id
+      body
+      isPublished
+      title
+      __typename
+      updatedAt
+      createdAt
+    }
+  }
+`;
+
 interface IPracticePageProps {}
 const PracticePage: React.FC<IPracticePageProps> = ({}) => {
   console.log(environmentVars);
+  const { data } = useQuery(DraftsDocument);
+  console.log(data);
   return (
     <div>
       <h1 className="text-red-400">this is the page changed 12345</h1>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
-import Head from "next/head";
-import { AppProps } from "next/app";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import theme from "theme";
-import createEmotionCache from "createEmotionCache";
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import createEmotionCache from 'createEmotionCache';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import theme from 'theme';
 
-import "styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import 'styles/globals.css';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -15,19 +15,25 @@ const clientSideEmotionCache = createEmotionCache();
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:8000',
+  cache: new InMemoryCache(),
+});
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <ApolloProvider client={apolloClient}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </ApolloProvider>
   );
 }
