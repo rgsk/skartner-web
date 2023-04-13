17aec11aa7d361a3a9da934bc24a3a8129ef0f8e
rgsk <guptarahul70322@gmail.com>
2023-04-13 13:33:25 +0530

    comments

--- codegen.ts
+++ codegen.ts
@@ -1,5 +1,10 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 require('dotenv').config();
+// above is required if variable is defined in local .env
+// docker env is provisioned without this package too
+
+// console.log('process.env.SKARTNER_SERVER', process.env.SKARTNER_SERVER);
+
 const config: CodegenConfig = {
   overwrite: true,
   schema: process.env.SKARTNER_SERVER,
