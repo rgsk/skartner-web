7199ff072405142fe161b883d18d9400ffe996fa
rgsk <guptarahul70322@gmail.com>
2023-04-30 22:56:43 +0530

    static dir for rendering images in public folder

--- src/components/Shared/MyButton/MyButton.tsx
+++ src/components/Shared/MyButton/MyButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@mui/material';
+import Image from 'next/image';
 
 interface IMyButtonProps {
   children: string;
@@ -8,6 +9,9 @@ const MyButton: React.FC<IMyButtonProps> = ({ children, onClick }) => {
   return (
     <div>
       <p className="text-purple-500">this is some random text</p>
+      <div className="border border-solid border-red-400 w-[100px] h-[100px] relative">
+        <Image src="/Background.png" alt="ss" fill />
+      </div>
       <Button onClick={onClick}>{children}</Button>
     </div>
   );
