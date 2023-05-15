d6c15b8bff30c88cdb0157c1000e6529c17d1c3f
rgsk <guptarahul70322@gmail.com>
2023-05-15 10:01:15 +0530

    minor

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -1,27 +1,16 @@
 import PracticePage from 'components/PracticePage/PracticePage';
-import { DraftsForPracticeDocument, DraftsForPracticeQuery } from 'gql/graphql';
-import apolloClient from 'lib/apolloClient';
 import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 
 export async function getStaticProps({}: GetStaticPropsContext) {
-  const { data } = await apolloClient.query<DraftsForPracticeQuery>({
-    query: DraftsForPracticeDocument,
-    fetchPolicy: 'no-cache',
-  });
-  console.log(data);
-
   return {
-    props: {
-      drafts: data,
-    },
+    props: {},
     revalidate: 10,
   };
 }
 
-const Page = ({ drafts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Page = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
-      {drafts && <div>{JSON.stringify(drafts)}</div>}
       <PracticePage />
     </div>
   );
