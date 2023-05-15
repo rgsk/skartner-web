57ce65d860cafd3e5e542d02b4e8ffc11fa4306b
rgsk <guptarahul70322@gmail.com>
2023-05-15 08:04:08 +0530

    proper pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Copyright from 'components/Sample/Copyright';
 import Link from 'components/Sample/Link';
-import ProTip from 'components/Sample/ProTip';
 
 export default function Home() {
   return (
@@ -17,18 +14,10 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI - Next.js example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
         <Link href="/gre" color="secondary">
           Gre Page
         </Link>
-        <Link href="/gre?name=rahul&page=3" color="secondary">
+        <Link href="/gre/history" color="secondary">
           Gre History Page
         </Link>
       </Box>
