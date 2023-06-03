c007828cb0f88b4e9fb1504b56e81ceb93d2ecbc
rgsk <guptarahul70322@gmail.com>
2023-06-03 09:51:49 +0530

    useWindowFocus detects tab changes

--- src/hooks/utils/useWindowFocus.ts
+++ src/hooks/utils/useWindowFocus.ts
@@ -1,24 +1,43 @@
 import { useEffect, useState } from 'react';
 
 export function useWindowFocus() {
-  const [isWindowFocused, setIsWindowFocused] = useState(hasFocus()); // Focus for first render.
+  const [isWindowFocused, setIsWindowFocused] = useState(
+    getInitialFocusState()
+  );
 
   useEffect(() => {
-    setIsWindowFocused(hasFocus()); // Focus for following renders.
+    setIsWindowFocused(getInitialFocusState());
 
     const onFocus = () => setIsWindowFocused(true);
     const onBlur = () => setIsWindowFocused(false);
 
+    const handleVisibilityChange = () => {
+      setIsWindowFocused(document.visibilityState === 'visible');
+    };
+
+    // this tracks window focus
     window.addEventListener('focus', onFocus);
     window.addEventListener('blur', onBlur);
+
+    // this tracks tab changes
+    document.addEventListener('visibilitychange', handleVisibilityChange);
 
     return () => {
       window.removeEventListener('focus', onFocus);
       window.removeEventListener('blur', onBlur);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, []);
 
   return { isWindowFocused };
+}
+
+function getInitialFocusState() {
+  return (
+    typeof window !== 'undefined' &&
+    document.visibilityState === 'visible' &&
+    hasFocus()
+  );
 }
 
 function hasFocus() {
