919eee6e2935c77df52b7c9f7bf3f4e805a913dd
rgsk <guptarahul70322@gmail.com>
2023-07-28 00:42:07 +0530

    minor

--- src/hooks/utils/useResizeObserver.ts
+++ src/hooks/utils/useResizeObserver.ts
@@ -1,4 +1,5 @@
-import { RefObject, useEffect, useState } from 'react';
+import { RefObject, useState } from 'react';
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
 // SAMPLE: SampleResizeObserver.tsx
 
@@ -7,9 +8,10 @@ const useResizeObserver = <T extends HTMLElement = HTMLElement>(
 ) => {
   const [boundingRect, setBoundingRect] = useState<DOMRect>();
 
-  useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     const element = elementRef.current;
     if (element) {
+      setBoundingRect(element.getBoundingClientRect());
       const resize_ob = new ResizeObserver(function (entries) {
         // since we are observing only a single element, so we access the first element in entries array
         let rect = entries[0].contentRect;
