eeb8ed4a1cc11023e31fce6f0ba8588a5e3dff52
rgsk <guptarahul70322@gmail.com>
2023-07-28 00:45:59 +0530

    resize observer comment

--- src/hooks/utils/useResizeObserver.ts
+++ src/hooks/utils/useResizeObserver.ts
@@ -8,10 +8,16 @@ const useResizeObserver = <T extends HTMLElement = HTMLElement>(
 ) => {
   const [boundingRect, setBoundingRect] = useState<DOMRect>();
 
+  // useIsomorphicLayoutEffect is used
+  // so that we use useLayoutEffect on the client side
+  // this ensures that rect is populated before we paint to the screen
   useIsomorphicLayoutEffect(() => {
     const element = elementRef.current;
     if (element) {
+      // setting below is necassary to ensure there is no delay in populating boundingRect
+      // and UI shows proper dimensions from the start
       setBoundingRect(element.getBoundingClientRect());
+
       const resize_ob = new ResizeObserver(function (entries) {
         // since we are observing only a single element, so we access the first element in entries array
         let rect = entries[0].contentRect;
