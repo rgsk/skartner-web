8042d6ade0037a436f0f2900039334df34de4945
rgsk <guptarahul70322@gmail.com>
2023-07-27 23:17:53 +0530

    useDebouncedEffect comment clarification

--- src/hooks/utils/useDebouncedEffect.ts
+++ src/hooks/utils/useDebouncedEffect.ts
@@ -1,5 +1,11 @@
 import { useEffect } from 'react';
 
+// SAMPLE: SampleDebounce.tsx
+
+// useDebouncedEffect doesn't provides the deps like interface
+// because we won't get eslint suggestions
+// it's better to pass a function wrapped in useCallback, and specify depedency in that
+
 const useDebouncedEffect = (func: () => void, delay = 500) => {
   useEffect(() => {
     const timer = setTimeout(func, delay);
