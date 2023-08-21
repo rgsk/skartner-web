f0a722955d9271f82905a5f5d4598854d46727b0
rgsk <guptarahul70322@gmail.com>
2023-08-21 14:16:16 +0530

    isIpad check should work in ipad chrome

--- src/constants/globalConstants.ts
+++ src/constants/globalConstants.ts
@@ -19,7 +19,19 @@ export const isTouchDevice =
     (navigator as any).msMaxTouchPoints > 0);
 // not sure where navigator.msMaxTouchPoints > 0 condition is useful
 
+// below are some sample user agents on ipad
+/*
+Chrome
+Mozilla/5.0 (iPad; CPU OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/115.0.5790.160 Mobile/15E148 Safari/604.1
+
+Safari
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15
+
+Webview
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)
+*/
 export const isIpad =
   typeof window !== 'undefined' &&
-  /Macintosh/i.test(navigator.userAgent) &&
+  (/Macintosh/i.test(navigator.userAgent) ||
+    /iPad/i.test(navigator.userAgent)) &&
   navigator.maxTouchPoints > 1;
